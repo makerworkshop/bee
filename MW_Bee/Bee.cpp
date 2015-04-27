e447c3bc2a15763e04c9794ac1975a3747fd3700
José Carlos Nieto <jose.carlos@menteslibres.net>
2015-04-26 20:29:31 -0500

    Adding documentation.

--- MW_Bee/Bee.cpp
+++ MW_Bee/Bee.cpp
@@ -1,63 +1,74 @@
 #include "Bee.h"
 #include "resp.h"
 
+// Bee constructor initializes variables that are used to determine the state
+// within Next.
 Bee::Bee()
 {
   this->cursor = 0;
 }
 
+// replyOK writes a status message with value RESP_OK to the connection
+// channel.
 void Bee::replyOK()
 {
   this->Write((unsigned char *)RESP_OK, 5);
 }
 
+// replyError writes a status message with value RESP_ERROR to the connection
+// channel.
 void Bee::replyError()
 {
   this->Write((unsigned char *)RESP_ERROR, 6);
 }
 
+// OnMessage implements default actions when receiving messages.
 respObject* Bee::OnMessage(respObject *in)
 {
   int pin;
 
-  // Basic control commands.
   if (RESP_TOKEN_EQUALS(in, 0, "SET")) {
     // SET {pin} {ON|OFF}
     pin = RESP_TOKEN_TO_INT(in, 1);
     pinMode(pin, OUTPUT);
-    if (RESP_TOKEN_EQUALS(in, 2, "ON")) {
+    if (RESP_TOKEN_EQUALS(in, 2, "1")) {
       digitalWrite(pin, HIGH);
     } else {
       digitalWrite(pin, LOW);
     }
+    // Returns status OK.
     return createRespString(RESP_OBJECT_STATUS, "OK");
   } else if (RESP_TOKEN_EQUALS(in, 0, "GET")) {
     // GET {pin}
     pin = RESP_TOKEN_TO_INT(in, 1);
     pinMode(pin, INPUT);
+    // Returns integer.
     return createRespInteger(digitalRead(pin));
   } else if (RESP_TOKEN_EQUALS(in, 0, "READ")) {
     // READ {pin}
     pin = RESP_TOKEN_TO_INT(in, 1);
     pinMode(pin, INPUT);
+    // Returns integer.
     return createRespInteger(analogRead(pin));
   } else if (RESP_TOKEN_EQUALS(in, 0, "WRITE")) {
     // WRITE {pin} {0-255 value}
     pin = RESP_TOKEN_TO_INT(in, 1);
     pinMode(pin, OUTPUT);
     analogWrite(pin, RESP_TOKEN_TO_INT(in, 2));
+    // Returns status OK.
     return createRespString(RESP_OBJECT_STATUS, "OK");
   }
 
+  // If nothing matched, return NULL, a NULL value will be catched by Next and
+  // transformed into an error.
   return NULL;
 }
 
-void Bee::Next()
+// NextMessage creates a RESP message with consecutive calls to Read.
+bool Bee::NextMessage()
 {
-  /* Listen for new data. */
-  int i;
+  int i, ok;
   char c;
-  int ok;
   bool gotLine;
 
   respObject *in = NULL;
@@ -65,20 +76,20 @@ void Bee::Next()
 
   gotLine = false;
 
-  /* Should we clean the buffer? */
+  // Cleaning initial buffer.
   if (this->cursor == 0) {
     for (i = 0; i < BUFLEN; i++) {
       this->buf[i] = 0;
     }
   }
 
-  /* Reading from serial port. */
+  // Reading one byte at a time and advancing cursor.
   for (gotLine = false; !gotLine && this->Read(&c); this->cursor++) {
 
     if (this->cursor >= BUFLEN) {
       this->replyError();
       this->cursor = 0;
-      return;
+      return false;
     }
 
     this->buf[this->cursor] = c;
@@ -90,8 +101,9 @@ void Bee::Next()
     }
   }
 
-  /* Processing line */
+  // Processing line.
   if (gotLine) {
+    // Attempt to decode message.
     ok = respDecode(&in, this->buf);
 
     if (ok > 0) {
@@ -100,7 +112,7 @@ void Bee::Next()
       freeRespObject(in);
 
       if (out == NULL) {
-        this->replyOK();
+        this->replyError();
       } else {
         ok = respEncode(out, this->buf);
         if (ok > 0) {
@@ -112,7 +124,9 @@ void Bee::Next()
       }
 
       this->cursor = 0;
+      return true;
     }
   }
-}
 
+  return false;
+}
--- MW_Bee/Bee.h
+++ MW_Bee/Bee.h
@@ -1,3 +1,8 @@
+/* Bee is a library that enables devices to communicate with other devices
+ * using RESP encoded messages over different user-implemented communication
+ * channels.
+ * */
+
 #if ARDUINO >= 100
 #include "Arduino.h"
 #else
@@ -6,32 +11,70 @@
 
 #include <resp.h>
 
+// RESP_TOKEN_LENGTH_AT returns the length of the token at the given index.
 #define RESP_TOKEN_LENGTH_AT(INPUT, INDEX)      ((INDEX < INPUT->elements) ? INPUT->element[INDEX]->len : 0)
+
+// RESP_TOKEN_AT returns a pointer to the token at the given index.
 #define RESP_TOKEN_AT(INPUT, INDEX)             ((INDEX < INPUT->elements) ? INPUT->element[INDEX]->str : (unsigned char *)"\0")
+
+// RESP_TOKEN_TO_INT returns the integer value of the token at the given index.
 #define RESP_TOKEN_TO_INT(INPUT, INDEX)         atoi((char*) RESP_TOKEN_AT(INPUT, INDEX))
+
+// RESP_TOKEN_EQUALS returns true if the value of the token at the given index
 #define RESP_TOKEN_EQUALS(INPUT, INDEX, CONST)  (memcmp(RESP_TOKEN_AT(INPUT, INDEX), (unsigned char *)CONST, RESP_TOKEN_LENGTH_AT(INPUT, INDEX)) == 0)
 
 #define RESP_OK     "+OK\r\n"
 #define RESP_ERROR  "-ERR\r\n"
 
-#define BUFLEN 128
+#define BUFLEN      256
 
+// Bee class defines virtual methods that can be satisfied by derived classes
+// that want to communicate using RESP formatted messages.
 class Bee {
-  private:
-    int cursor;
-    unsigned char buf[BUFLEN];
-    void replyOK();
-    void replyError();
+private:
+  int cursor;
+  unsigned char buf[BUFLEN];
+  void replyOK();
+  void replyError();
 
-  public:
-    Bee();
-    ~Bee();
+public:
+  Bee();
 
-    void Next();
+  // NextMessage attempts to read the next message, if the message is
+  // incomplete, it will exit and return the control to the caller. Returns
+  // true when a complete message was received, false otherwise.
+  bool NextMessage();
 
-    virtual respObject* OnMessage(respObject*);
-    virtual bool Open()=0;
-    virtual bool Read(char *)=0;
-    virtual bool Write(unsigned char *, int)=0;
-    virtual bool Close()=0;
+  // Open attempts to configure the communication channel and to leave it ready
+  // for reading and writing. Returns true on success, false otherwise.
+  virtual bool Open()=0;
+
+  // Read attempts to read a byte from the communication channel and to write
+  // it to <b>. Returns true on success, false otherwise.
+  virtual bool Read(char *b)=0;
+
+  // Write attempts to write <len> bytes to the communication channel, the
+  // source for the write is <buf>. Returns true on success, false otherwise.
+  virtual bool Write(unsigned char *buf, int len)=0;
+
+  // Close closes the communication channel. Returns true on success, false
+  // otherwise.
+  virtual bool Close()=0;
+
+  // OnMessage executes predefined actions based on the following messages:
+  //
+  // SET {PIN NUMBER} {1|0}
+  // SET performs a digital write to the given PIN. A value of 1 means HIGH
+  // while 0 means LOW.
+  //
+  // GET {PIN NUMBER}
+  // GET performs a digital read from the given PIN. Returns either 0 or 1 for
+  // HIGH or LOW respectively.
+  //
+  // WRITE {PIN NUMBER} {0 - 255} WRITE performs an analog write to the given
+  // PIN. Accepts a value between 0 and 255.
+  //
+  // READ {PIN NUMBER} READ performs an analog read from the given PIN. Returns
+  // a value between 0 and 1023.
+  virtual respObject* OnMessage(respObject*);
 };
